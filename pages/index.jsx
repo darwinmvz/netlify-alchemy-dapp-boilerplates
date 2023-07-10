a21a264fd236602f66232534cc09a25fd08ebcc3
DARWIN MAURICIO VEGA ZAMBRANO <83559669+darwinmvz@users.noreply.github.com>
2023-07-10 09:38:06 -0500

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.s";
 import InstructionsComponent from "../components/InstructionsComponent";
 
 export default function Home() {
